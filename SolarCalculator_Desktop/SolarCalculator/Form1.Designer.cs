f6264491a65ac2e7a07ffc1b364c5eeff51226ef
Brendon Park <brendon.park@bigpond.com>
2012-09-04 23:20:12 +1000

    Desktop Application - Initial Commit

---
+++ SolarCalculator_Desktop/SolarCalculator/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace SolarCalculator
+{
+    partial class GUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNumberOfPanels = new System.Windows.Forms.TextBox();
+            this.txtInverterEfficiency = new System.Windows.Forms.TextBox();
+            this.txtPanelEfficiency = new System.Windows.Forms.TextBox();
+            this.txtSystemSize = new System.Windows.Forms.TextBox();
+            this.txtDaytimeUsage = new System.Windows.Forms.TextBox();
+            this.txtHoursOfDaylight = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.txtImportPrice = new System.Windows.Forms.TextBox();
+            this.txtExportPrice = new System.Windows.Forms.TextBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(310, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of Panels";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(400, 410);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 40);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.Calculate);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hours of Daylight";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(166, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Daytime Usage (KWh)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(310, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Panel Output (KWh)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(310, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Panel Efficiency (%)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(310, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(163, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Inverter Efficiency (%)";
+            // 
+            // txtNumberOfPanels
+            // 
+            this.txtNumberOfPanels.Location = new System.Drawing.Point(485, 130);
+            this.txtNumberOfPanels.Name = "txtNumberOfPanels";
+            this.txtNumberOfPanels.Size = new System.Drawing.Size(100, 20);
+            this.txtNumberOfPanels.TabIndex = 9;
+            // 
+            // txtInverterEfficiency
+            // 
+            this.txtInverterEfficiency.Location = new System.Drawing.Point(485, 100);
+            this.txtInverterEfficiency.Name = "txtInverterEfficiency";
+            this.txtInverterEfficiency.Size = new System.Drawing.Size(100, 20);
+            this.txtInverterEfficiency.TabIndex = 10;
+            // 
+            // txtPanelEfficiency
+            // 
+            this.txtPanelEfficiency.Location = new System.Drawing.Point(485, 70);
+            this.txtPanelEfficiency.Name = "txtPanelEfficiency";
+            this.txtPanelEfficiency.Size = new System.Drawing.Size(100, 20);
+            this.txtPanelEfficiency.TabIndex = 11;
+            // 
+            // txtSystemSize
+            // 
+            this.txtSystemSize.Location = new System.Drawing.Point(485, 40);
+            this.txtSystemSize.Name = "txtSystemSize";
+            this.txtSystemSize.Size = new System.Drawing.Size(100, 20);
+            this.txtSystemSize.TabIndex = 12;
+            // 
+            // txtDaytimeUsage
+            // 
+            this.txtDaytimeUsage.Location = new System.Drawing.Point(185, 70);
+            this.txtDaytimeUsage.Name = "txtDaytimeUsage";
+            this.txtDaytimeUsage.Size = new System.Drawing.Size(100, 20);
+            this.txtDaytimeUsage.TabIndex = 13;
+            // 
+            // txtHoursOfDaylight
+            // 
+            this.txtHoursOfDaylight.Location = new System.Drawing.Point(185, 40);
+            this.txtHoursOfDaylight.Name = "txtHoursOfDaylight";
+            this.txtHoursOfDaylight.Size = new System.Drawing.Size(100, 20);
+            this.txtHoursOfDaylight.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(124, 420);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(203, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Power Generated (KWh)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(579, 420);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(192, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Money Saved/Made ($)";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(10, 460);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(198, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Power Generated Per Day:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(10, 490);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(215, 20);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Power Generated Per Month:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(10, 520);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(204, 20);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "Power Generated Per Year:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(460, 520);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(175, 20);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Money Saved Per Year:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(460, 490);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(186, 20);
+            this.label13.TabIndex = 21;
+            this.label13.Text = "Money Saved Per Month:";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(460, 460);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(169, 20);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "Money Saved Per Day:";
+            // 
+            // txtImportPrice
+            // 
+            this.txtImportPrice.Location = new System.Drawing.Point(185, 100);
+            this.txtImportPrice.Name = "txtImportPrice";
+            this.txtImportPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtImportPrice.TabIndex = 26;
+            // 
+            // txtExportPrice
+            // 
+            this.txtExportPrice.Location = new System.Drawing.Point(185, 130);
+            this.txtExportPrice.Name = "txtExportPrice";
+            this.txtExportPrice.Size = new System.Drawing.Size(100, 20);
+            this.txtExportPrice.TabIndex = 25;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(10, 130);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(163, 20);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "Export Price (¢ / KWh)";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(10, 100);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(163, 20);
+            this.label16.TabIndex = 23;
+            this.label16.Text = "Import Price (¢ / KWh)";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(368, 9);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(165, 20);
+            this.label17.TabIndex = 27;
+            this.label17.Text = "System Information";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(65, 9);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(170, 20);
+            this.label18.TabIndex = 28;
+            this.label18.Text = "General Information";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label19.Location = new System.Drawing.Point(675, 9);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(151, 20);
+            this.label19.TabIndex = 29;
+            this.label19.Text = "Other Information";
+            // 
+            // GUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 612);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.txtImportPrice);
+            this.Controls.Add(this.txtExportPrice);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtHoursOfDaylight);
+            this.Controls.Add(this.txtDaytimeUsage);
+            this.Controls.Add(this.txtSystemSize);
+            this.Controls.Add(this.txtPanelEfficiency);
+            this.Controls.Add(this.txtInverterEfficiency);
+            this.Controls.Add(this.txtNumberOfPanels);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label1);
+            this.Location = new System.Drawing.Point(300, 20);
+            this.Name = "GUI";
+            this.Text = "Solar Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtNumberOfPanels;
+        private System.Windows.Forms.TextBox txtInverterEfficiency;
+        private System.Windows.Forms.TextBox txtSystemSize;
+        private System.Windows.Forms.TextBox txtDaytimeUsage;
+        private System.Windows.Forms.TextBox txtHoursOfDaylight;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox txtImportPrice;
+        private System.Windows.Forms.TextBox txtExportPrice;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox txtPanelEfficiency;
+    }
+}
+
---
+++ SolarCalculator_Desktop/SolarCalculator/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace SolarCalculator
+{
+    public partial class GUI : Form
+    {
+        const string TemplateOfflineUri = "http://localhost:8888/outputxml.jsp?numpanels={0}&daylighthours={1}&hourlyusage={2}&paneloutput={3}&panelefficiency={4}&inverterefficiency={5}";
+        const string TemplateOnlineUri = "http://solarpanelcalc.appspot.com/outputxml.jsp?numpanels={0}&daylighthours={1}&hourlyusage={2}&paneloutput={3}&panelefficiency={4}&inverterefficiency={5}";
+
+        public GUI()
+        {
+            InitializeComponent();
+        }
+
+        private string GenerateUriString()
+        {
+            return string.Format(TemplateOnlineUri, txtNumberOfPanels.Text, txtHoursOfDaylight.Text, txtDaytimeUsage.Text, txtSystemSize.Text, txtPanelEfficiency.Text, txtInverterEfficiency.Text);
+        }
+        private void Calculate(object sender, EventArgs e)
+        {
+            string xmlInput = HttpMethods.Request(new Uri(GenerateUriString()), RequestMethod.GET);
+
+            if (xmlInput == null)
+            {
+                MessageBox.Show("There was an error connecting to the server");
+                return;
+            }
+
+            xmlInput = xmlInput.Remove(0, xmlInput.IndexOf('>'));
+
+            string systemOutput;
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlInput)))
+            {
+                reader.ReadToFollowing("output");
+                systemOutput = reader.ReadElementContentAsString();
+            }
+
+            label9.Text = "Power Generated Per Day: " + systemOutput;
+        }
+    }
+}
