0f3068757209f4dd2f0ad95ce376ad841126bed4
Sean Burnett <sean_zygoma@hotmail.com>
2012-09-18 19:49:28 +1000

    Cost GUI

--- SolarCalculator_Desktop/SolarCalculator/CostCalculation.cs
+++ SolarCalculator_Desktop/SolarCalculator/CostCalculation.cs
@@ -16,24 +16,93 @@ namespace SolarCalculator
     {
         
         public delegate void StringDelegate(string input);
-
-        //Todo: Const Strings, don't know how to use
-
-        /*
-         * const string TemplateOfflineUri = "http://localhost:8888/outputxml.jsp?";
-         * const string TemplateOnlineUri = "http://solarpanelcalc.appspot.com/outputxml.jsp?";
-         */
-
-
+        
+        const string TemplateOfflineUri = "http://localhost:8888/outputxml.jsp?numpanels={0}&paneloutput{1}";
+        const string TemplateOnlineUri = "http://solarpanelcalc.appspot.com/outputxml.jsp?numpanels={0}&paneloutput{1}";
+         
         public CostCalculation()
         {
             InitializeComponent();
         }
 
         //Combine inputs into a URL string to use in the HttpMethods.Request
-        private string GenerateUriString() {
+        private string GenerateUriString()
+        {
+            return string.Format(TemplateOnlineUri, numNumPan.Text, numPanOut.Text);
+        }
 
-            //return string.Format;
+        private void QueryServer(object uriTarget)
+        {
+            string uriString = uriTarget as string;
+            if (uriString == null)
+                throw new Exception("Cannot be null.");
+
+            string xmlInput = RESTMethods.Request(new Uri(uriString),RequestMethod.GET);
+
+            //Checking response
+            if (xmlInput == null)
+            {
+                MessageBox.Show("There was an error connecting to the server.");
+                CleanUp();
+                return;
+            }
+
+            xmlInput = xmlInput.Remove(0, xmlInput.IndexOf('>') + 1);
+
+            string systemCost; //Creating xmlReader to parse through response
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlInput)))
+            {
+                reader.ReadToFollowing("cost");
+                systemCost = reader.ReadElementContentAsString();
+            }
+            Output(systemCost);
+        }
+
+        private void Output(string stringToOutput)
+        {
+            if (lblSysCost.InvokeRequired)
+            {
+                StringDelegate call = new StringDelegate(Output);
+                BeginInvoke(call, stringToOutput);
+            }
+            else
+            {
+                lblSysCost.Text = stringToOutput.ToString();
+                CleanUp();
+            }       
+        }
+
+        private void CleanUp() 
+        {
+            if (lblSysCost.InvokeRequired)
+                BeginInvoke(new MethodInvoker(CleanUp));
+            else
+            {
+                btnCalc.Enabled = true;
+            }        
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            var MainMenu = new MainMenu();
+            this.Hide();
+            MainMenu.Show();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            btnCalc.Enabled = false;
+
+            ParameterizedThreadStart startInfo = new ParameterizedThreadStart(QueryServer);
+            Thread thread = new Thread(startInfo);
+            thread.IsBackground = true;
+
+            thread.Start(GenerateUriString());
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
 
         private void CostCalculation_FormClosing(object sender, FormClosingEventArgs e)
