43ace5633ac7ebd2649072e4607ffd07e9da77c1
KierrenSmall <kierren.small@connect.qut.edu.au>
2012-10-11 14:38:57 +1000

    Mobile detection and redirection

--- SolarPowerCalculator_AppEngine/src/solarPowerCalculator/SolarPowerCalculatorServlet.java
+++ SolarPowerCalculator_AppEngine/src/solarPowerCalculator/SolarPowerCalculatorServlet.java
@@ -1,17 +1,52 @@
 package solarPowerCalculator;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class SolarPowerCalculatorServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        
+    	
+    	//Get User Data from header
+    	String userAgent = req.getHeader("User-Agent");
+    	
+    	//If User Data contains Android or iPhone, redirect to mobile version
+    	if (userAgent.contains("Android") || userAgent.contains("iPhone")) {
+    		//Print Header info, for debugging
+    		/*resp.setContentType("text/plain");
+        	resp.getWriter().println(req.getHeader("User-Agent"));*/
+    		
+    		//Redirect to mobile version
+    		resp.sendRedirect("indexm.jsp");
+    	}
+    	else {
+    		//Redirect to desktop version
+    		resp.sendRedirect("index.jsp");
+    	}
+    	
     }
     
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    	
+    	//Get User Data from header
+    	String userAgent = req.getHeader("User-Agent");
+    	
+    	//If User Data contains Android or iPhone, redirect to mobile version
+    	if (userAgent.contains("Android") || userAgent.contains("iPhone")) {
+    		//Print Header info, for debugging
+    		/*resp.setContentType("text/plain");
+        	resp.getWriter().println(req.getHeader("User-Agent"));*/
+    		
+    		//Redirect to mobile version
+    		resp.sendRedirect("indexm.jsp");
+    	}
+    	else {
+    		//Redirect to desktop version
+    		resp.sendRedirect("index.jsp");
+    	}
     	
     }
 }
