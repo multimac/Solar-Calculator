2e35fff0c44a7709732f238e357dc0629336ef3f
KierrenSmall <kierren.small@connect.qut.edu.au>
2012-10-16 18:19:17 +1000

    Cleaned up a little

--- SolarPowerCalculator_AppEngine/war/js/validatesolaroutput.js
+++ SolarPowerCalculator_AppEngine/war/js/validatesolaroutput.js
@@ -1,75 +1,91 @@
-//Placeholder file for javascript validation
+
+var errorMessages = ["Number of panels should be a number between 1 and 50.",
+                     "Monthly consumption should be a number between 0 and 1000.",
+                     "Panel Output should be a number between 50 and 500.",
+                     "Panel Degradation should be a number between 0 and 100.",
+                     "Inverter Efficiency should be a number between 1 and 100.",
+                     "Daylight Hours should be a number between 1 and 24.",
+                     "Temperature should be a number between -10 and 50.",
+                     "Export Tariff should be a number between 0.01 and 1."
+                    ];
+
+const errNumPanels = 0;
+const errMonthlyConsumption = 1;
+const errPanelOutput = 2;
+const errPanelDegradation = 3;
+const errInverterEfficiency = 4;
+const errDaylighthours = 5;
+const errTemperature = 6;
+const errExportRate = 7;
 
 //Main function calling validation rules
 function formValidation() {
+	
+	
+	
 	
 	var inputError = false;
 	var errorList = new Array();
 	
 	if(!validNumPanels()) {
 		inputError = true;
-		errorList.push("<li>Number of panels should be a number between 1 and 50.</li>");
+		errorList.push("<li>" + errorMessages[errNumPanels] + "</li>");
 	}
 	
 	if(!validMonthlyConsumption()) {
 		inputError = true;
-		errorList.push("<li>Monthly consumption should be a number between 0 and 1000.</li>");
+		errorList.push("<li>" + errorMessages[errMonthlyConsumption] + "</li>");
 	}
 	
 	
 	
 	if (!validPanelOutput()) {
 		inputError = true;
-		errorList.push("<li>Panel Output should be a number between 50 and 500.</li>");
+		errorList.push("<li>" + errorMessages[errPanelOutput] + "</li>");
 	}
 	
-	if (!validPercentage(document.output.panelefficiency.value)) {
+	if (!validPanelDegredation()) {
 		inputError = true;
-		errorList.push("<li>Panel Degradation should be a number between 0 and 100.</li>");
+		errorList.push("<li>" + errorMessages[errPanelDegradation] + "</li>");
 	}
 	
-	if (!validPercentage(document.output.inverterefficiency.value)) {
+	if (!validInverterEfficiency()) {
 		inputError = true;
-		errorList.push("<li>Inverter Efficiency should be a number between 0 and 100.</li>");
+		errorList.push("<li>" + errorMessages[errInverterEfficiency] + "</li>");
 	}
 	
 	
 	
-	if (!validDaylightHours(document.output.daylighthours.value)) {
+	if (!validDaylightHours()) {
 		inputError = true;
-		errorList.push("<li>Daylight Hours should be a number between 1 and 24.</li>");
+		errorList.push("<li>" + errorMessages[errDaylighthours] + "</li>");
 	}
 	
-	if (!validTemperature(document.output.temperature.value)) {
+	if (!validTemperature()) {
 		inputError = true;
-		errorList.push("<li>Temperature should be a number between -10 and 50.</li>");
+		errorList.push("<li>" + errorMessages[errTemperature] + "</li>");
 	}
 	
-	if (!validExportRate(document.output.exportrate.value)) {
+	if (!validExportRate()) {
 		inputError = true;
-		errorList.push("<li>Export Tariff should be a number between 0.01 and 1.</li>");
+		errorList.push("<li>" + errorMessages[errExportRate] + "</li>");
 	}
 	
 	
 	// Error Handling: outputs all errors that occured
 	if (inputError == true) {
-		
 		var errorBoxStart = '<div id="error"><h3>The following errors occured during calculation:</h3><ul>';
 		var errorBoxEnd = "</ul></div>";
 		var errorString = "";
 		
 		for (errorIndex in errorList) {
-		
 			errorString += errorList[errorIndex];
-		
 		}
 		document.getElementById("diverroroutput").innerHTML = errorBoxStart + errorString + errorBoxEnd;
 		return false;
 	}
 	else {
-	
 		return true;
-
 	}
 }
 
@@ -93,6 +109,7 @@ function validMonthlyConsumption() {
 }
 
 
+
 function validPanelOutput() {
 	var panelOutput = document.output.paneloutput.value;
 	if ((panelOutput >= 50) && (panelOutput <= 500) && isNaN(panelOutput) == false) {
@@ -100,6 +117,24 @@ function validPanelOutput() {
 	}
 	return false;
 }
+
+function validPanelDegredation() {
+	var panelDegredation = document.output.panelefficiency.value;
+	if ((panelDegredation >= 0) && (panelDegredation <= 100) && isNaN(panelDegredation) == false) {
+		return true;
+	}
+	return false;
+}
+
+function validInverterEfficiency() {
+	var inverterEfficiency = document.output.inverterefficiency.value;
+	if ((inverterEfficiency >= 1) && (inverterEfficiency <= 100) && isNaN(inverterEfficiency) == false) {
+		return true;
+	}
+	return false;
+}
+
+
 
 function validDaylightHours() {
 	var daylighthours = document.output.daylighthours.value;
@@ -116,6 +151,8 @@ function validTemperature() {
 	}
 	return false;
 }
+
+
 
 function validExportRate() {
 	var exportrate = document.output.exportrate.value;
